a43b282ee8de5a1371d78f5842cce0cde58034cd
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-02-15 11:22:03 -0300

    created note front and back end

--- app/Livewire/Pages/Notes/Create.php
+++ app/Livewire/Pages/Notes/Create.php
@@ -2,10 +2,54 @@
 
 namespace App\Livewire\Pages\Notes;
 
+use App\Models\Note;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Create extends Component
 {
+    public $user;
+
+    #[Validate('required|min:3|max:255')]
+    public string $title = '';
+
+    #[Validate('required|min:3|max:255')]
+    public string $content = '';
+
+    public function mount()
+    {
+        $this->user = auth()->user();
+
+        if (!$this->user) {
+            redirect()->route('login');
+        }
+    }
+
+    protected function messages()
+    {
+        return [
+            'title.required' => 'O título é obrigatório.',
+            'content.required' => 'O conteúdo é obrigatório.',
+            'title.min' => 'O título precisa ter pelo menos 3 caracteres.',
+            'content.min' => 'O conteúdo precisa ter pelo menos 3 caracteres.',
+            'title.max' => 'O título pode ter no máximo 255 caracteres.',
+            'content.max' => 'O conteúdo pode ter no máximo 255 caracteres.'
+        ];
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        Note::create(
+          $this->only(['title', 'content'])
+        );
+
+        //session()->flash('status', 'Nota criada com sucesso.');
+
+        $this->redirect(route('dashboard.notes'));
+    }
+
     public function render()
     {
         return view('livewire.pages.notes.create')
--- resources/views/livewire/pages/notes/create.blade.php
+++ resources/views/livewire/pages/notes/create.blade.php
@@ -1,5 +1,4 @@
 <div>
-
     <div class="text-center">
         <h1 class="text-xl md:text-2xl md:pt-10 text-black-accent font-medium">
             Crie sua nota
@@ -7,9 +6,9 @@
     </div>
 
     <div class="flex flex-col justify-center items-center mt-5">
-        <form>
+        <form wire:submit.prevent="save">
             <div class="relative">
-                <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 mb-7 pointer-events-none">
                     <x-svg.pencil
                         class="w-5 h-5 text-gray-accent"
                     />
@@ -19,31 +18,42 @@
                     id="title"
                     class="block mt-1 w-full"
                     type="text"
+                    wire:model="title"
                     name="title"
-                    required autofcus
                     autocomplete="user"
                 />
+
+                <div class="mt-2 text-red-500 text-sm h-5">
+                    @error('title') <span class="error">{{ $message }}</span>@enderror
+                </div>
             </div>
 
             <div class="relative mt-3">
-                <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none mb-80 md:mb-80">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none mb-[350px]">
                     <x-svg.notes
                         class="w-5 h-5 text-gray-accent"
                     />
                 </div>
 
                 <textarea
-                    id="message"
+                    id="content"
+                    type="text"
+                    wire:model="content"
+                    name="content"
                     rows="4"
-                    class="block p-4 px-10 py-5 md:py-7 w-full h-96 md:h-[400px] text-sm border-gray-300 focus:border-primary focus:primary rounded-md shadow-sm"></textarea>
+                    class="block p-4 px-10 py-5 md:py-7 w-full h-96 md:h-[400px] text-sm border-gray-300 focus:border-primary focus:primary rounded-md shadow-sm">
+                </textarea>
+
+                <div class="mt-2 text-red-500 text-sm h-5">
+                    @error('content') <span class="error">{{ $message }}</span>@enderror
+                </div>
             </div>
 
             <div class="mt-3">
-                <x-primary-button>
+                <x-primary-button type="submit">
                     Salvar
                 </x-primary-button>
             </div>
         </form>
     </div>
-
 </div>
