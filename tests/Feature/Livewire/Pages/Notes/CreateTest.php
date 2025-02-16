cbd876bcd968d97632a3dffcccd626b95faf1801
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-02-15 21:02:53 -0300

    test: should be able validate camp title and content

--- tests/Feature/Livewire/Pages/Notes/CreateTest.php
+++ tests/Feature/Livewire/Pages/Notes/CreateTest.php
@@ -27,3 +27,29 @@ it('should be able to create notes', function () {
         ->assertRedirect(route('dashboard.notes'))
         ->assertSuccessful();
 });
+
+it('should be able validate camp title and content', function () {
+    $user = User::factory()->create();
+
+    actingAs($user);
+
+    Livewire::test(Create::class)
+        ->set('title', '')
+        ->assertHasErrors(['title' => 'required'])
+        ->call('save')
+        ->set('content', '')
+        ->assertHasErrors(['content' => 'required'])
+        ->call('save')
+        ->set('title', 'Te')
+        ->assertHasErrors(['title' => 'min'])
+        ->call('save')
+        ->set('content', 'Te')
+        ->assertHasErrors(['content' => 'min'])
+        ->call('save')
+        ->set('title', str_repeat('a', 256))
+        ->assertHasErrors(['title' => 'max'])
+        ->call('save')
+        ->set('content', str_repeat('a', 256))
+        ->assertHasErrors(['content' => 'max'])
+        ->call('save');
+});
