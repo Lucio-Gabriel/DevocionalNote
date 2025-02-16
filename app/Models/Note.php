55b3a896e21165878a84283c55e8347ad898ce74
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-02-15 22:04:10 -0300

    feat: created uuid to note

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Note extends Model
 {
@@ -13,4 +14,20 @@ class Note extends Model
         'title',
         'content',
     ];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model)
+        {
+           if (empty($model->id))
+           {
+               $model->id = (string) Str::uuid();
+           }
+        });
+    }
 }
