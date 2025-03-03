4e32d589b9bfb9624aba0d8f30d3e82a54866397
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-03-03 12:08:10 -0300

    refactor: updated routes

--- routes/web.php
+++ routes/web.php
@@ -4,29 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::get('dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+Route::get('dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('dashboard/pomodoro', \App\Livewire\Pomodoro\Index::class)
-    ->middleware(['auth', 'verified'])
-    ->name('pomodoro.index');
+Route::get('dashboard/pomodoro', \App\Livewire\Pomodoro\Index::class)->name('pomodoro.index');
 
-Route::get('dashboard/notes', App\Livewire\Notes\Index::class)
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard.notes');
+Route::get('dashboard/notes', App\Livewire\Notes\Index::class)->name('dashboard.notes');
 
-Route::get('dashboard/notes/create', \App\Livewire\Notes\Create::class)
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard.notes.create');
+Route::get('dashboard/notes/create', \App\Livewire\Notes\Create::class)->name('dashboard.notes.create');
 
-Route::get('dashboard/notes/{note}/edit', \App\Livewire\Notes\Edit::class)
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard.notes.edit');
+Route::get('dashboard/notes/{note}/edit', \App\Livewire\Notes\Edit::class)->name('dashboard.notes.edit');
 
-Route::get('dashboard/notes/{note}', \App\Livewire\Notes\Show::class)
-    ->middleware(['auth', 'verified'])
-    ->name('notes.show');
+Route::get('dashboard/notes/{note}', \App\Livewire\Notes\Show::class)->name('notes.show');
+});
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
